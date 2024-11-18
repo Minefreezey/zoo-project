f73c18739a86e05fc0a14683768c5bab9db2a43f
Wongsatorn Sungsilpawech <wongsatorn.sung@hotmail.com>
2024-11-18 15:58:24 +0000

    Fixed the condition that can result in other bugs

--- zoo.py
+++ zoo.py
@@ -2,11 +2,11 @@ class Zoo:
     def get_ticket_price(self, age):
         if age < 0:
             return -1
-        elif 0 <= age <= 12:
+        elif age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif age <= 20:
             return 100
-        elif 21 < age <= 60:
+        elif age <= 60:
             return 150
-        elif age >= 60:
+        else:
             return 100
